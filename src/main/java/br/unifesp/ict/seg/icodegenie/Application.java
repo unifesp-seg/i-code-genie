513fc3525925cc4de1971ca5eeea66a36cfc383d
adrianocarv <adrianocarv@yahoo.com.br>
2019-04-25 18:48:07 -0300

    Version 1.0.2-RC-01

--- src/main/java/br/unifesp/ict/seg/icodegenie/Application.java
+++ src/main/java/br/unifesp/ict/seg/icodegenie/Application.java
@@ -1,8 +1,12 @@
 package br.unifesp.ict.seg.icodegenie;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import br.unifesp.ict.seg.geniesearchapi.infrastructure.util.GenieSearchAPIConfig;
 
 /**
  * The entry point of the Spring Boot application.
@@ -10,8 +14,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
+        GenieSearchAPIConfig.loadProperties(args);
     }
-
 }
